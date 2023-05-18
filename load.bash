ecce77ed144a7389ffb46f29c37e1fa73f081309
David G Rosenberg <targendaz2@me.com>
2023-05-19 05:55:50 +0800

    Initial files for zset

--- load.bash
+++ load.bash
@@ -1,2 +1,3 @@
-source "$(dirname "${BASH_SOURCE[0]}")/src/zrun.bash"
 source "$(dirname "${BASH_SOURCE[0]}")/src/zsource.bash"
+source "$(dirname "${BASH_SOURCE[0]}")/src/zset.bash"
+source "$(dirname "${BASH_SOURCE[0]}")/src/zrun.bash"
---
+++ test/zset.bats
@@ -0,0 +1,6 @@
+#!/usr/bin/env bats
+
+bats_require_minimum_version 1.5.0
+
+load test_helper
+load '../load'
