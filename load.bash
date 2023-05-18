39f830465a8152002e5c24388e5387b9aa84b1a5
David G Rosenberg <targendaz2@me.com>
2023-05-19 06:02:50 +0800

    Adjusted load order

--- load.bash
+++ load.bash
@@ -1,3 +1,3 @@
 source "$(dirname "${BASH_SOURCE[0]}")/src/zsource.bash"
-source "$(dirname "${BASH_SOURCE[0]}")/src/zset.bash"
 source "$(dirname "${BASH_SOURCE[0]}")/src/zrun.bash"
+source "$(dirname "${BASH_SOURCE[0]}")/src/zset.bash"
